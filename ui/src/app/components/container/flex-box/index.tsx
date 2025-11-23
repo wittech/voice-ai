1df70250b5bb8deaf5fc89885509580f0c444541
Prashant Srivastav <p_srivastav@outlook.com>
2025-11-23 14:39:16 +0800

    feat: fixed oauth2 authentication

--- ui/src/app/components/container/flex-box/index.tsx
+++ ui/src/app/components/container/flex-box/index.tsx
@@ -12,7 +12,7 @@ export function FlexBox(props: FlexBoxProps) {
   return (
     <main
       {...attrs}
-      className="relative antialiased dark:text-gray-400 bg-gray-50 dark:bg-gray-950 h-screen flex flex-col flex-1"
+      className="relative antialiased dark:text-gray-400 bg-gray-50 dark:bg-gray-950 min-h-[100dvh] flex flex-col flex-1"
     >
       <Header />
       <div className="flex flex-col flex-1 grow">{props.children}</div>
