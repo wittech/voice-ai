445e6bacaa116ac694ec3cf3a3db25e60c414380
Prashant Srivastav <p_srivastav@outlook.com>
2026-02-06 23:55:02 +0800

    chore: cleanup code

--- api/assistant-api/api/talk/talk.go
+++ api/assistant-api/api/talk/talk.go
@@ -11,6 +11,7 @@ import (
 	"github.com/rapidaai/api/assistant-api/config"
 	internal_adapter "github.com/rapidaai/api/assistant-api/internal/adapters"
 	internal_grpc "github.com/rapidaai/api/assistant-api/internal/channel/grpc"
+	internal_webrtc "github.com/rapidaai/api/assistant-api/internal/channel/webrtc"
 	internal_services "github.com/rapidaai/api/assistant-api/internal/services"
 	internal_assistant_service "github.com/rapidaai/api/assistant-api/internal/services/assistant"
 	web_client "github.com/rapidaai/pkg/clients/web"
@@ -126,6 +127,42 @@ func (cApi *ConversationGrpcApi) AssistantTalk(stream assistant_api.TalkService_
 		return errors.New("illegal source")
 	}
 	streamer, err := internal_grpc.NewGrpcStreamer(stream.Context(), cApi.logger, stream)
+	if err != nil {
+		cApi.logger.Errorf("failed to create grpc streamer: %v", err)
+		return err
+	}
+	talker, err := internal_adapter.GetTalker(
+		source,
+		stream.Context(),
+		cApi.cfg,
+		cApi.logger,
+		cApi.postgres,
+		cApi.opensearch,
+		cApi.redis,
+		cApi.storage,
+		streamer,
+	)
+	if err != nil {
+		cApi.logger.Errorf("failed to setup talker: %v", err)
+		return err
+	}
+
+	return talker.Talk(stream.Context(), auth, internal_adapter.Identifier(source, stream.Context(), auth, ""))
+}
+
+func (cApi *ConversationGrpcApi) WebTalk(stream assistant_api.WebRTC_WebTalkServer) error {
+	auth, isAuthenticated := types.GetSimplePrincipleGRPC(stream.Context())
+	if !isAuthenticated {
+		cApi.logger.Errorf("unable to resolve the authentication object, please check the parameter for authentication")
+		return errors.New("unauthenticated request for messaging")
+	}
+
+	source, ok := utils.GetClientSource(stream.Context())
+	if !ok {
+		cApi.logger.Errorf("unable to resolve the source from the context")
+		return errors.New("illegal source")
+	}
+	streamer, err := internal_webrtc.NewWebRTCStreamer(stream.Context(), cApi.logger, stream)
 	if err != nil {
 		cApi.logger.Errorf("failed to create grpc streamer: %v", err)
 		return err
